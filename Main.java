9f9ca05de9db41b8a6c6b9f1481983831b53a3f3
Reaper <md.saadbinnaeem@gmail.com>
2026-08-06 17:46:20 +0600

    Changed Main and Added Main2

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, GitHub from Linux Mint!");
+        System.out.println("Hello, GitHub from Windows 11!");
     }
 }
